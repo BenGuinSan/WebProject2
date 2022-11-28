1a5b85de56b1583e4bb997cbb570b8819aeb09ef
Vu <tranngocvu4220@gmail.com>
2022-11-28 22:07:56 +0700

    Sua lai admin panel

--- admin.js
+++ admin.js
@@ -1,10 +1,4 @@
-import { products } from "./ProductList.js";
-
-
-
-
-
-
+/*import { products } from "./ProductList.js";
 
 var a=localStorage.getItem("ProductList");
 var productList = JSON.parse(a);
@@ -26,32 +20,54 @@ function displayProductList(productList)
     });
     document.getElementById('innerProductList').innerHTML = s.join('');
 }
-displayProductList(productList);
+displayProductList(productList);*/
 
-/* function displayAccountList() {
-    return `
-    <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="editButton"><a href="#">Sửa</a> / <a href="#">Xóa</a></td>
-    </tr>
-    `
-    document.getElementById('innerAccount').innerHTML = s.join('');
+function renderProduct(){
+    let jsonProduct= localStorage.getItem('ProductList') ? JSON.parse(localStorage.getItem('ProductList')) : []
+    let productdb =`<tr>
+        
+    </tr>` 
+
+    jsonProduct.map((value,index)=>{
+        productdb +=`<tr>
+        <td>${index}</td>
+        <td>${value.name}</td>
+        <td>${value.category}</td>
+        <td>${value.price}</td>
+        <td>
+            <button onclick="deleteProduct(${index})">Delete</button>
+            <button onclick="editProduct(${index})">Edit</button>
+        </td>
+    </tr>    
+    ` 
+    })
+
+    document.getElementById('innerProductList').innerHTML=productdb
 }
-displayAccountList();
 
-function displaySoldList() {
-    return `
-    <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-    `
-    document.getElementById('innerSold').innerHTML = s.join('');
+function renderAccount(){
+
+    let userArray= localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : []
+    let userdb =`<tr>
+        
+    </tr>` 
+
+    userArray.map((value,index)=>{
+        userdb +=`<tr>
+        <td>${index+1}</td>
+        <td>${value.username}</td>
+        <td>${value.email}</td>
+        <td>
+            <button onclick="deleteProduct(${index})">Delete</button>
+        </td>
+    </tr>    
+    ` 
+    })
+
+    document.getElementById('innerAccountList').innerHTML=userdb
 }
-displaySoldList(); */
 
+window.onload=function(){
+    renderAccount()
+    renderProduct()
+}
